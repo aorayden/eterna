cc4b03eb8888a4995f243a1a5627b346b770ba8a
aorayden <aorayden@icloud.com>
2025-12-01 17:12:01 +0500

    Add placeholder screens
    Added placeholder ImportScreen and ExportScreen widgets. Updated .gitignore to include build/ directory.

---
+++ preview/lib/screens/export_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ExportScreen extends StatelessWidget {
+  const ExportScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
