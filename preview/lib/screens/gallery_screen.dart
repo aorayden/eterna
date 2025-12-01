9de8da3a7ff0825d783ac60750008e4e25abfa24
aorayden <aorayden@icloud.com>
2025-12-01 17:17:48 +0500

    Update gallery_screen.dart

--- preview/lib/screens/gallery_screen.dart
+++ preview/lib/screens/gallery_screen.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:ui_kit/components/search.dart';
 
 class GalleryScreen extends StatelessWidget {
   const GalleryScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [AppSearch(onChanged: (value) {})],
+        ),
+      ),
+    );
   }
 }
