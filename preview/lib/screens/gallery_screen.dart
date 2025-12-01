7cba726342c71a8068a51a00a49308f69d29f696
aorayden <aorayden@icloud.com>
2025-12-01 18:14:34 +0500

    Update gallery_screen.dart

--- preview/lib/screens/gallery_screen.dart
+++ preview/lib/screens/gallery_screen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:ui_kit/components/button.dart';
 import 'package:ui_kit/components/search.dart';
+import 'package:ui_kit/models/button_enums.dart';
+import 'package:ui_kit/theme/colors.dart';
+import 'package:ui_kit/theme/text_styles.dart';
 
 class GalleryScreen extends StatelessWidget {
   const GalleryScreen({super.key});
@@ -10,7 +14,16 @@ class GalleryScreen extends StatelessWidget {
       child: Scaffold(
         body: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: [AppSearch(onChanged: (value) {})],
+          children: [
+            AppSearch(onChanged: (value) {}),
+            Text(
+              'Каталог произведений',
+              style: AppTextStyles.thirdTitleSemibold(
+                color: AppColors.placeholder,
+              ),
+            ),
+            AppButton(text: 'ВсеASDAS', kind: AppButtonKind.small),
+          ],
         ),
       ),
     );
